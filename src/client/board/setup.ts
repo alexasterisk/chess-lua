91664a077775d845251a4da6877b3435200299eb
alex <31458482+alexinite@users.noreply.github.com>
2022-03-10 12:08:51 -0500

    add alphabet and fix exports

--- src/client/board/setup.ts
+++ src/client/board/setup.ts
@@ -1,10 +1,8 @@
-const emptyRow = [];
+export const emptyRow = [];
 
-const standardRankRow = [ "r", "n", "b", "k", "q", "b", "n", "r" ];
-const standardWallRow = [ "p", "p", "p", "p", "p", "p", "p", "p" ];
+export const standardRankRow = [ "r", "n", "b", "k", "q", "b", "n", "r" ];
+export const standardWallRow = [ "p", "p", "p", "p", "p", "p", "p", "p" ];
 
 export const standard = [ standardRankRow, standardWallRow, emptyRow, emptyRow, emptyRow, emptyRow, standardWallRow, standardRankRow ];
 
-export emptyRow;
-export standardRankRow;
-export StandardWallRow;
+export const alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
