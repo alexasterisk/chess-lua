6ffad4faf021be1eda6ff5370036a6ad6a9f995b
alex <31458482+alexinite@users.noreply.github.com>
2022-03-10 12:13:52 -0500

    add ownership per row identifiers

--- src/client/board/setup.ts
+++ src/client/board/setup.ts
@@ -4,5 +4,6 @@ export const standardRankRow = [ "r", "n", "b", "k", "q", "b", "n", "r" ];
 export const standardWallRow = [ "p", "p", "p", "p", "p", "p", "p", "p" ];
 
 export const standard = [ standardRankRow, standardWallRow, emptyRow, emptyRow, emptyRow, emptyRow, standardWallRow, standardRankRow ];
+export const ownership = [ "w", "w", "", "", "", "", "b", "b" ];
 
 export const alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
