7a74b4e1e905e3e549489e1f7038189f9533c9ba
alex <31458482+alexinite@users.noreply.github.com>
2022-03-10 11:51:07 -0500

    Create setup.ts

---
+++ src/client/board/setup.ts
@@ -0,0 +1,10 @@
+const emptyRow = [];
+
+const standardRankRow = [ "r", "n", "b", "k", "q", "b", "n", "r" ];
+const standardWallRow = [ "p", "p", "p", "p", "p", "p", "p", "p" ];
+
+export const standard = [ standardRankRow, standardWallRow, emptyRow, emptyRow, emptyRow, emptyRow, standardWallRow, standardRankRow ];
+
+export emptyRow;
+export standardRankRow;
+export StandardWallRow;
