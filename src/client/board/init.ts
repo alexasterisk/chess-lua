6855448c888ec983541502d3b9ba08bb17dd52e7
alex <31458482+alexinite@users.noreply.github.com>
2022-03-10 12:14:58 -0500

    formatting

--- src/client/board/init.ts
+++ src/client/board/init.ts
@@ -1,6 +1,6 @@
 const size = { x: 8, y: 8 };
 const color = [ new Color3(1, 1, 1), new Color3(0, 0, 0) ];
-import { alphabet, standard } from "./setup";
+import { alphabet, standard, ownership } from "./setup";
 
 // end custom data
 
@@ -15,14 +15,18 @@ const boardFrame = new Instance("Frame");
 boardFrame.Name = "Holder";
 
 const boardData = new Map<>();
+
 for (const row = 1, row < size.y, row++) {
   const rowName = alphabet[row];
   const inverted = row % 2 == 0;
+
   for (const column = 1, column < size.x, column++) {
     const bgColor = inverted ? color[!(column % 2)] : color[column % 2];
     const identifier = rowName + column;
     const piece = standard[row][column];
     const owner = ownership[row];
+    
+    
   }
 }
 
