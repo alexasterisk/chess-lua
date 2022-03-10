5b07514954918ce59e92679f5c021bf6b3b85fb1
alex <31458482+alexinite@users.noreply.github.com>
2022-03-10 12:05:52 -0500

    Update init.ts

--- src/client/board/init.ts
+++ src/client/board/init.ts
@@ -1,2 +1,27 @@
 const size = { x: 8, y: 8 };
 const color = [ new Color3(1, 1, 1), new Color3(0, 0, 0) ];
+import standard as layout from "./setup";
+
+// end custom data
+
+import Players from "@rbxts/services";
+const player = Players.LocalPlayer;
+const playerGui = player.WaitForChild("PlayerGui");
+
+const boardGui = new Instance("ScreenGui");
+boardGui.Name = "ChessBoard";
+
+const boardFrame = new Instance("Frame");
+boardFrame.Name = "Holder";
+
+const boardData = new Map<>();
+for (const row = 1, row < size.y, row++) {
+  const inverted = row % 2 == 0;
+  for (const column = 1, column < size.x, column++) {
+    let bgColor = inverted ? color[!(column % 2)] : color[column % 2];
+  }
+}
+
+boardFrame.Parent = boardGui;
+
+boardGui.Parent = playerGui;
